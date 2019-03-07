b0d94ed70bb5772cd2610c2bd9ccae0696b4b3be
joseelizondo22 <jose22je@outlook.com>
2019-03-07 10:11:14 -0600

    code

--- weekOneCode/Program.cs
+++ weekOneCode/Program.cs
@@ -9,7 +9,55 @@ namespace weekOneCode
     class Program
     {
         static void Main(string[] args)
-        {
+            {
+                int i = 10, j = 20;
+
+                if (i > j)
+                {
+                    Console.WriteLine("i is greater than j");
+                }
+
+                if (i < j)
+                {
+                    Console.WriteLine("i is less than j");
+                }
+
+                if (i == j)
+                {
+                    Console.WriteLine("i is equl to j");
+                }
+
+                int x = 10;
+
+                switch (x)
+                {
+                    case 5:
+                        Console.WriteLine("Value of x is 5");
+                        break;
+                    case 10:
+                        Console.WriteLine("Value of x is 10");
+                        break;
+                    case 15:
+                        Console.WriteLine("Value of x is 15");
+                        break;
+                    default:
+                        Console.WriteLine("Unknown value");
+                        break;
+                }
+
+                do
+                {
+                    Console.WriteLine("Vaule of i: {0}", i);
+                    i++;
+                    if (i > 5)
+                        break;
+                } while (true);
+                int[] intArray = new int[5] { 10, 20, 30, 40, 50 };
+
+                for (int a = 0; i < intArray.Length; i++)
+                    Console.WriteLine(intArray[i]);
+            }
+
         }
     }
-}
+
